9b00604ab0ff6aa42a17060b929384740fb3a5f9
Max Lim <maxlim33@icloud.com>
2026-05-27 09:14:40 -0400

    update console script

--- jira/src/main/resources/com/example/ScratchScript.groovy
+++ jira/src/main/resources/com/example/ScratchScript.groovy
@@ -5,4 +5,17 @@ package com.example
 "If instead you'd like to run your scripts directly from IntelliJ IDEA, " +
 "take a look at: https://scriptrunner.adaptavist.com/latest/jira/DevEnvironment.html#_external_tool_for_running_scripts_against_jira."
 
-Issues.getByKey('SS-2').summary
+import com.atlassian.jira.event.type.EventDispatchOption
+import com.atlassian.jira.issue.MutableIssue
+import com.atlassian.jira.component.ComponentAccessor
+
+def JQL = '''assignee is not EMPTY'''
+def count = Issues.count(JQL)
+def i = 0
+Issues.search(JQL).each { issue ->
+    def mutableIssue = ComponentAccessor.issueManager.getIssueByCurrentKey(issue.key) as MutableIssue
+    mutableIssue.setAssignee(null)
+    ComponentAccessor.issueManager.updateIssue(Users.loggedInUser, mutableIssue, EventDispatchOption.ISSUE_UPDATED, false)
+    i++
+    log.warn "Unassign issue successfully for $i/$count"
+}
